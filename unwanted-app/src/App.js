037544ba160149d4243566558fa6b92b6c9d5efa
Rhys Swansbra <r.l.swansbra@gmail.com>
2021-10-08 18:43:04 +1100

    started using components for scalability

--- unwanted-app/src/App.js
+++ unwanted-app/src/App.js
@@ -1,47 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import NavigationBaby from './NavigationBaby';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Button} from 'react-bootstrap'
-import {Navbar} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Shop</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <NavigationBaby />
 
     </div>
   );
